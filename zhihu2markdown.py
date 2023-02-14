80e1b30518a50af7e2eedbd8c1c45d11586e18b2
Saltsmart <40828952+Saltsmart@users.noreply.github.com>
2023-02-14 15:49:34 +0800

    Update zhihu2markdown.py
    默认保存文件名直接换成文章标题（可能有文件名非法字符？）

--- zhihu2markdown.py
+++ zhihu2markdown.py
@@ -6,7 +6,7 @@ import os
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Transform zhihu article to Markdown format')
 	parser.add_argument('article_url', help='URL of zhihu article')
-	parser.add_argument('-o', '--output', help='path of output markdown file', default='./a.md')
+	parser.add_argument('-o', '--output', help='path of output markdown file')
 	parser.add_argument('-i', '--image_dir', help='If present, download image to the image dir path')
 	parser.add_argument('-a', '--user_agent', help='User agent')
 
@@ -18,13 +18,14 @@ if __name__ == '__main__':
 		config.asset_path = args.image_dir
 	if args.user_agent:
 		config.user_agent = args.user_agent
-	
+
 	article_pattern = r'https://zhuanlan.zhihu.com/p/(\d.+)/?'
 	objmatch = re.search(article_pattern, args.article_url)
 	if not objmatch.group(1):
-		raise "Article URL not match. Must like: https://zhuanlan.zhihu.com/p/1234567"
+		raise "Article URL not match. Must be: https://zhuanlan.zhihu.com/p/xxxxxxx"
 
 	article = Article(objmatch.group(1), config)
-	output_path = os.path.expanduser(args.output)
+	output_path = args.output if args.output else './' + article.title + ".md"
+	output_path = os.path.expanduser(output_path)
 	with open(output_path, 'w') as output_file:
 		output_file.write(article.markdown)
